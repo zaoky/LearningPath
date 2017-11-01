01bef4ea227f807b5f0f59deea8466678f402e78
Alexander Rojas (zaoky) <proxtos@gmail.com>
2017-11-01 12:37:30 -0600

    Add project files.

---
+++ Learning Path/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Learning_Path.Startup))]
+namespace Learning_Path
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
