5df719f5bf47b67703431108b495173db58c21f9
Alexander Rojas (zaoky) <proxtos@gmail.com>
2017-11-03 16:55:10 -0600

    cap 15

--- Learning Path/App_Start/RouteConfig.cs
+++ Learning Path/App_Start/RouteConfig.cs
@@ -12,6 +12,10 @@ namespace Learning_Path
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
+            //routes.MapRoute("MoviesByReleaseDate", "movies/released/{year}/{month}", new { Controller = "Movies", Action = "ByReleaseDate"},
+            //   // new { year = @"\d{4}", month = @"\d{2}"}); for limite year just at four digits
+            //    new { year = @"2015|2016", month = @"\d{2}"}); // limite just for 2015 or 2016 years
 
             routes.MapRoute(
                 name: "Default",
--- Learning Path/Controllers/MoviesController.cs
+++ Learning Path/Controllers/MoviesController.cs
@@ -14,5 +14,11 @@ namespace Learning_Path.Controllers
             };
             return View(movie);
         }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")] //constraints atribute route
+        public ActionResult ByReleaseDate(int year, byte month)
+        {
+            return Content($"{year}/{month}");
+        }
     }
 }
