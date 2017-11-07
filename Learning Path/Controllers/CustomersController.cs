6ccc3b19475eab3be3eb1f9e0614bebec1760d40
Alexander Rojas (zaoky) <proxtos@gmail.com>
2017-11-07 16:20:17 -0600

    class 21

---
+++ Learning Path/Controllers/CustomersController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Learning_Path.Models;
+
+namespace Learning_Path.Controllers
+{
+    public class CustomersController : Controller
+    {
+        readonly List<Customer> Customers = new List<Customer>
+            {
+                new Customer {Id=1,  Name = "Jhon Smith"},
+                new Customer {Id=2, Name = "Mary Williams"}
+            };
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(Customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Customer cust = this.Customers.FirstOrDefault(c => c.Id == id);
+            return View(cust ?? new Customer {Id = -1, Name = "Customers"});
+        }
+    }
+}
--- Learning Path/Controllers/MoviesController.cs
+++ Learning Path/Controllers/MoviesController.cs
@@ -1,35 +1,22 @@
 ﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using Learning_Path.Models;
-using Learning_Path.ViewModels;
 
 namespace Learning_Path.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ActionResult Random()
+        public ActionResult Index()
         {
-            Movie movie = new Movie()
+            List<Movie> movie = new List<Movie>
             {
-                Name = "Shrek!"
+                new Movie { Id=1, Name = "Shrek" },
+                new Movie { Id=2, Name = "Wall-e" }
             };
-
-            List<Customer> customers = new List<Customer>
-            {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"},
-                new Customer {Name = "Customer 3"},
-                new Customer {Name = "Customer 4"},
-            };
-
-            RandomMovieViewModel viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-            return View(viewModel);
+            return View(movie);
         }
+        
+        
 
         [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")] //constraints atribute route
         public ActionResult ByReleaseDate(int year, byte month)
